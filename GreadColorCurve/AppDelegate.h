67b0a8f2b9f8089efe31dba6dd5afe338bf159b0
zhangkang <zhangkangjiqi@163.com>
2017-07-31 14:32:59 +0800

    Initial Commit

---
+++ GreadColorCurve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreadColorCurve
+//
+//  Created by LSH on 2017/7/31.
+//  Copyright © 2017年 Practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreadColorCurve/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GreadColorCurve
+//
+//  Created by LSH on 2017/7/31.
+//  Copyright © 2017年 Practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
